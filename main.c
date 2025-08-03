ea6be9fe8624d40fff7a012efa500ab50dfa5602
Brendan Pelto <peltobrendan@yahoo.com>
2025-08-02 21:08:31 -0500

    Cleanup to main.c

--- main.c
+++ main.c
@@ -12,14 +12,15 @@
 #define CCSDS_SECONDARY_HEADER_CUC_TIME_SIZE 8
 #define CCSDS_SECONDARY_HEADER_TC_PUS_SIZE 3
 
-int main(int argc, char *argv[])
+int validate_args(int argc, char *argv[])
 {
-    // EVENTUALLY VALIDATE THE ARGC and ARGV
     if(argc < 2) {
         printf("Please pass a command for secondary header\n");
         return EXIT_FAILURE;
     } 
-    else if (strcmp(argv[1], "CUC_TIME") == 0)
+    
+    ccsds_sec_header_types_t cmd = parse_command(argv[1]);
+    if (cmd == CUC_TIME)
     {
         if(argc < 4 || argc > 4)
         {
@@ -27,13 +28,56 @@ int main(int argc, char *argv[])
             return EXIT_FAILURE;
         }
     }
+    else if (cmd == TC_PUS)
+    {
+        if(argc < 5 || argc > 5)
+        {
+            printf("Need only  arguments when sending TC_PUS secondary header cmd\n");
+            return EXIT_FAILURE;
+        }
+    }
+    return CCSDS_OK;
+}
 
-    // else if here to check argv[1] later 
+int validate_build_sec_header(ccsds_secondary_header_t *sec_header, int argc, char *argv[])
+{
+    ccsds_sec_header_types_t cmd = parse_command(argv[1]);
 
-    ccsds_primary_header_t header;
+    if (cmd == CMD_NONE) {
+        printf("(CUC_TIME, TC_PUS) are the only valid cmds at the moment\n");
+        return EXIT_FAILURE;
+    }
+
+
+    // Assign type directly based on enum
+    sec_header->type = (cmd == CUC_TIME) ? CCSDS_SEC_CUC_TIME : CCSDS_SEC_TC_PUS;
+
+    ccsds_error_t result = build_secondary_header(sec_header, argc, argv);
+    if (result != CCSDS_OK) {
+        printf("Error building secondary header\n");
+        return EXIT_FAILURE;
+    }
+
+    if (result != CCSDS_OK)
+    {
+        printError(result);
+        return result;
+    }
+
+    return CCSDS_OK;
+}
+
+int main(int argc, char *argv[])
+{
+    if(validate_args(argc, argv) != 0)
+    {
+        return EXIT_FAILURE;
+    }
+
     uint8_t *raw;
     size_t raw_size;
 
+    ccsds_primary_header_t header;
     int32_t version = 0;
     int32_t type = 1;
     int32_t sec_hdr_flag = 1;
@@ -52,38 +96,11 @@ int main(int argc, char *argv[])
 
     // Validate and build secondary header if flag is set
     ccsds_secondary_header_t sec_header = {0};
-    int include_sec_hdr = 0;
-    if (header.sec_hdr_flag == 1)
-    {
-        ccsds_sec_header_types_t cmd = parse_command(argv[1]);
-
-        if (cmd == CMD_NONE) {
-            printf("(CUC_TIME, TC_PUS) are the only valid cmds at the moment\n");
-            return EXIT_FAILURE;
-        }
-
-        include_sec_hdr = 1;
-
-        // Assign type directly based on enum
-        sec_header.type = (cmd == CUC_TIME) ? CCSDS_SEC_CUC_TIME : CCSDS_SEC_TC_PUS;
-
-        ccsds_error_t result = build_secondary_header(&sec_header, argc, argv);
-        if (result != CCSDS_OK) {
-            printf("Error building secondary header\n");
-            return EXIT_FAILURE;
-        }
-
-        if (result != CCSDS_OK)
-        {
-            printError(result);
-            return result;
-        }
-    }
-
     size_t sec_header_len;
-    // Maybe calloc later?
-    if (include_sec_hdr)
+    if(header.sec_hdr_flag == 1)
     {
+        validate_build_sec_header(&sec_header, argc, argv);
+
         switch(sec_header.type)
         {
             case(CCSDS_SEC_CUC_TIME):
@@ -103,17 +120,17 @@ int main(int argc, char *argv[])
         {
             perror("malloc failed");
             return 1;
-        }
+        }        
     }
-    else
-    {
+    else {
+        sec_header_len = 0;
         raw_size = CCSDS_PRIMARY_HEADER_SIZE;
         raw = malloc(raw_size);
         if (!raw)
         {
             perror("malloc failed");
             return 1;
-        }
+        }        
     }
 
     // Encode
